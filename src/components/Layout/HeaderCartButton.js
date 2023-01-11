06411b38fb015ab56e1d88c81e0d7ec6ecc5742c
tylergan <tgan2615@gmail.com>
2023-01-11 17:44:20 +1100

    Fixed the issue of the Add to Cart button bump animation not playing if we spam the cart button

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -20,12 +20,25 @@ export default function HeaderCartButton(props) {
 
         const timer = setTimeout(() => { // reset the button animation after 300ms
             setBtnIsHighlighted(false)
-        }, 300)
+        }, 200)
 
         return () => { // clean up function
             clearTimeout(timer) // reset the timer if the component is re-rendered
+            // if we spam the add to cart button, the order of the state update is not guaranteed to be in order (since we are using a timing function)
+            // so, sometimes, the button will not be highlighted since the setBtnIsHighlighted(false) is never called and will remain true.
         }
     }, [cartCtx.items])
+
+    useEffect(() => { // thus, we need to use a second useEffect to reset the button animation; so, if the state remains true, we will reset the animation back to false
+        if (btnIsHighlighted) {
+            const timer = setTimeout(() => {
+                setBtnIsHighlighted(false)
+            }, 200)
+            return () => {
+                clearTimeout(timer)
+            }
+        }
+    }, [btnIsHighlighted])  // thus, spamming the add to cart button will not have any animation issues since we ensure that the state will always be reset to false
 
     return (
         <button className={btnClasses} onClick={props.onShowCart}>
